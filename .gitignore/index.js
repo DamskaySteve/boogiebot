a4849e68d9aa1f3c0347d1498d1fda65148510d2
DamskaySteve <37937235+DamskaySteve@users.noreply.github.com>
2018-06-01 00:06:36 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -4,7 +4,7 @@ const bot = new Discord.Client();
 var prefix = ("!")
 
 bot.on('ready', function() {
-    bot.user.setGame("Bot pour tets");
+    bot.user.setGame("Bot pour Luminex");
     console.log("Connecter");
 });
 
@@ -14,10 +14,6 @@ bot.login(process.env.TOKEN)
 bot.on('message', message =>{
     if (message.content ==="Salut"){
         message.reply("Salut a toi :)");
-        console.log("Commande reply salut faite !")
-	    
-        if (message.content ==="Yo"){
-        message.reply("Yo mon rheys !");
         console.log("Commande reply salut faite !")
     }
 });
