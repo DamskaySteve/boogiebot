14a45372463e1f08952937813173f403c8a00338
DamskaySteve <37937235+DamskaySteve@users.noreply.github.com>
2018-06-01 00:03:25 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -4,7 +4,7 @@ const bot = new Discord.Client();
 var prefix = ("!")
 
 bot.on('ready', function() {
-    bot.user.setGame("Bot pour Luminex");
+    bot.user.setGame("Bot pour tets");
     console.log("Connecter");
 });
 
